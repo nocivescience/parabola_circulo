18a12458d82f59928cad23672c4c61678f719915
noceive <nocivescience@gmail.com>
2022-07-13 19:40:37 -0400

    recien empezando

--- parabola_y_cicrculo.py
+++ parabola_y_cicrculo.py
@@ -1,1 +1,13 @@
 from manim import *
+class ParabolaScene(Scene):
+    CONFIG={
+        'config_axes':
+            'x_range':(-5,5),
+            'y_range':(0,10),
+    }
+    def construct(self):
+        
+    def get_axes_and_graph(self):
+        axes = Axes(
+            
+        )
